0f659a5beaec5c6b463790c96fc4a44a544e6467
Nathan Polomark <polomarkn@hotmail.com>
2020-11-26 18:53:31 -0800

    Get all posts, create post, delete post working, update post not working yet

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const apiRouter = require("./routes/index");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+apiRouter(app);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/gardenapp");
