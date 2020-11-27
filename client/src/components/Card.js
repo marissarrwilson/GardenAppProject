d92e70f39410b20fca485f9093e9095df64db854
Mars <wilson.marissarose@gmail.com>
2020-11-27 14:29:25 -0500

    Edited Card.js and added Card.css file

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,33 +1,52 @@
-import React from "react"
-
+import React from "react";
+import "./Card.css";
 
 // Post Title (is this required???)
 // Crop Status
 // Square Footage
 // Quantity
 // Harvest Weight
-// Date Started 
+// Date Started
 // Date Planted
 // Date Harvested
 // Field Notes
 
+function Card(props) {
+  return (
+    <div className="card" style={{ width: "22rem" }}>
+      <img src="https://picsum.photos/200/150" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h3 className="card-title">{props.title}New Title</h3>
 
+        <p className="plantName">
+          Crop Planted: {props.plantName}
+        </p>
 
-function Card(props){
-    return(
+        <p className="card-text">
+          Planted on {props.datePlanted}__ over {props.sqFootage}__
+          sq.ft.
+        </p>
 
-<div className="card" style={{width: '18rem'}}>
-  <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{props.plant}</h5>
-    <p className="card-text">Planted on {props.datestarted} over {props.squarefootage}.</p>
-    <p className="card-text">{props.fielnotes}</p>
-    <p className="card-text">{props.fielnotes}</p>
-    <a href="#" className="btn btn-primary">{props.viewdetails}button here later change</a>
-  </div>
-</div>
+        <p className="card-text">Status: {props.status}</p>
 
-    );
+        <p className="card-text">
+          Field Notes: {props.fieldNotes}
+        </p>
+
+        <div className="row">
+          <a href="/post" className="btn btn-success col-5">
+            {props.viewdetails} Edit Post
+          </a>
+
+          <div className="col-2"></div>
+
+          <a href="/feed" className="btn btn-danger">
+            {props.viewdetails} Delete
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
