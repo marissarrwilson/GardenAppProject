1846f619f1f3400b9a0b5d9bc0d28126a5fcb470
Learningitnow <70935742+Learningitnow@users.noreply.github.com>
2020-11-30 11:00:27 -0500

    change bootstrcap to browser router navlink

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import "./Header.css";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import UserContext from "../context/UserContext";
-import { useHistory } from "react-router-dom";
+import { useHistory,NavLink } from "react-router-dom";
+
 
 
 export default function Header(props) {
@@ -28,14 +29,14 @@ export default function Header(props) {
           <h1><center>Get Growing <i class="fas fa-seedling"></i></center></h1>
           <h5>Your Gardening e-Journal</h5>
         {/* <Button className="navBtn" href="/pantry" variant="secondary" active >My Pantry</Button> */}
-        <Button className="navBtn" href="/feed" variant="secondary" active>My Feed</Button>
-        <Button className="navBtn" href ="/post" variant="danger" active>+ New Post</Button>
+        <NavLink className="navBtn" to="/feed" variant="secondary" active>My Feed</NavLink>
+        <NavLink className="navBtn" to ="/post" variant="danger" active>+ New Post</NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="navLinks ml-auto mt-0 mt-lg-0">
           {userData.user ? (
-            <button onClick={logout} href="#">Log out</button>
+            <Nav.Link onClick={logout} href="/">Log out</Nav.Link>
           ) : (
             <div>
               <Nav.Link onClick={login} >Log in</Nav.Link>
